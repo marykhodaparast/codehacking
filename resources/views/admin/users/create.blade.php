3f8cd5cd00b2f72bafb131cb61f8c75439cc1d31
maryam khodaparast <khcomputergirl@gmail.com>
2020-11-11 21:21:19 +0330

    Adding upload file feature to form 27-207

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <h1>Create Users</h1>
-    {!! Form::open(['method' => 'post', 'action' => 'AdminUsersController@store']) !!}
+    {!! Form::open(['method' => 'post', 'action' => 'AdminUsersController@store','files' => true]) !!}
     <div class="form-group">
         {!! Form::label('name','Name') !!}
         {!! Form::text('name',null,['class' => 'form-control']) !!}
@@ -21,6 +21,10 @@
         {!! Form::label('status','Status') !!}
         {!! Form::select('status',array(1=>'Active',0=> 'Not Active'), 0 ,['class' => 'form-control']) !!}
     </div>
+     <div class="form-group">
+          {!! Form::label('file','File') !!}
+          {!! Form::file('file',null,['class' => 'form-control']) !!}
+      </div>
      <div class="form-group">
           {!! Form::label('password','Password') !!}
           {!! Form::password('password',['class' => 'form-control']) !!}
