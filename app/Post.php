5694068e2f7d6d7babd6921e5f4cb62b75764ab6
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-19 21:12:00 +0330

    Displaying Post 28-227

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'body',
+        'category_id',
+        'photo_id',
+
+    ];
 }
