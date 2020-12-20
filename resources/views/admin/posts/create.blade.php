4b592829426f93adbcca57f75c0bf67966a3a815
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-20 22:59:59 +0330

    creating form part1 28-229

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,4 +1,26 @@
 @extends('layouts.admin')
 @section('content')
-   <h1>Create posts</h1>
+    <h1>Create posts</h1>
+    {!! Form::open(['method' => 'post', 'action' => 'AdminPostsController@store','files' => true]) !!}
+    <div class="form-group">
+        {!! Form::label('title','Title') !!}
+        {!! Form::text('title',null,['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('category_id','Category:') !!}
+        {!! Form::select('category_id',array('' => 'options'),null,['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('photo_id','Photo:') !!}
+        {!! Form::file('photo_id',null,['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('body','Description:') !!}
+        {{--        {!! Form::textarea('body',null,['class' => 'form-control','rows' => 3]) !!}--}}
+        {!! Form::textarea('body',null,['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::submit('Create Post',['class' => 'btn btn-primary']) !!}
+    </div>
+    {!! Form::close() !!}
 @endsection
