3433905c5d5cd722a8130e4ca865e454f8e6d37d
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-21 15:21:17 +0330

    Displaying images in post 28-234

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,14 +4,14 @@
     <table class="table">
         <thead>
         <tr>
-           <th>Id</th>
-           <th>Owner</th>
-           <th>Category</th>
-           <th>Photo</th>
-           <th>Title</th>
-           <th>Body</th>
-           <th>Created</th>
-           <th>Updated</th>
+            <th>Id</th>
+            <th>Photo</th>
+            <th>Owner</th>
+            <th>Category</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th>Created</th>
+            <th>Updated</th>
         </tr>
         </thead>
         <tbody>
@@ -19,9 +19,10 @@
             @foreach($posts as $post)
                 <tr>
                     <td>{{$post->id}}</td>
+                    <td><img height="50" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}"
+                             alt=""></td>
                     <td>{{$post->user->name}}</td>
                     <td>{{$post->category_id}}</td>
-                    <td>{{$post->photo_id}}</td>
                     <td>{{$post->title}}</td>
                     <td>{{$post->body}}</td>
                     <td>{{$post->created_at->diffForHumans()}}</td>
