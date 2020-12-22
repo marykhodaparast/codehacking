1cc0ae1cb18a3af83654bcff0ec885b6e7cb74bc
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-22 13:16:58 +0330

    Editing post part 1  28-239

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.admin')
-
 @section('content')
-    <h1>Edit Posts</h1>
+    <h1>Edit post</h1>
+    <div class="row">
+        {!! Form::model(['method' => 'PATCH', 'action' => ['AdminPostsController@update',$post->id],'files' => true]) !!}
+        <div class="form-group">
+            {!! Form::label('title','Title') !!}
+            {!! Form::text('title',null,['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('category_id','Category:') !!}
+            {!! Form::select('category_id', $categories ,null,['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('photo_id','Photo:') !!}
+            {!! Form::file('photo_id',null,['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('body','Description:') !!}
+            {{--        {!! Form::textarea('body',null,['class' => 'form-control','rows' => 3]) !!}--}}
+            {!! Form::textarea('body',null,['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Create Post',['class' => 'btn btn-primary']) !!}
+        </div>
+        {!! Form::close() !!}
+    </div>
+    <div class="row">
+        @include('includes.form_error')
+    </div>
+
 @endsection
