3ab2eddb9cb26d72df799ad19d231b33e72c4b62
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-24 14:36:24 +0330

    Deleting images 30-252

--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -16,6 +16,13 @@
                     <td>{{ $photo->id }}</td>
                     <td><img src="{{$photo->file}}" alt="" height="50"></td>
                     <td>{{$photo->created_at ? $photo->created_at->diffForHumans() : 'no date'}}</td>
+                    <td>
+                        {!! Form::open(['method' => 'DELETE', 'action' => ['AdminMediasController@destroy',$photo->id]]) !!}
+                        <div class="form-group">
+                            {!! Form::submit('Delete',['class' => 'btn btn-danger']) !!}
+                        </div>
+                        {!! Form::close() !!}
+                    </td>
                 </tr>
             @endforeach
             </tbody>
