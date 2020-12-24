a663e02d524a77ea4d77befecfa13f4b80587b50
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-24 12:51:29 +0330

    Adding the upload plugin 30-250

--- resources/views/admin/media/create.blade.php
+++ resources/views/admin/media/create.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.admin')
+
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/min/dropzone.min.css"
+          type="text/css">
+@endsection
+
 @section('content')
     <h1>Upload Media</h1>
+    {!! Form::open(['method' => 'post', 'action' => 'AdminMediasController@store','class' => 'dropzone']) !!}
+    {!! Form::close() !!}
+@endsection
 
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/min/dropzone.min.js"></script>
 @endsection
