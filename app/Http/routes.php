b092ea38955ada5de09e7d0f901772600da7c622
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-25 15:52:25 +0330

    Setting up part1 31-253

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,4 +30,6 @@ Route::group(['middleware' => 'admin'],function (){
     Route::resource('admin/categories','AdminCategoriesController');
     Route::resource('admin/media','AdminMediasController');
     Route::any('admin/media/upload',['as' => 'admin.media.upload', 'uses' => 'AdminMediasController@store']);
+    Route::resource('admin/comments','PostCommentsController');
+    Route::resource('admin/comment/replies','CommentRepliesController');
 });
