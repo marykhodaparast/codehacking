b8ed0c8a4fe02bc7369c09f28955d0a9986e6793
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-26 19:15:14 +0330

    Displaying comments 31-263

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -10,6 +10,8 @@
             <th>Category</th>
             <th>Title</th>
             <th>Body</th>
+            <th>Post</th>
+            <th>Comment</th>
             <th>Created</th>
             <th>Updated</th>
         </tr>
@@ -25,6 +27,8 @@
                     <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
                     <td>{{$post->title}}</td>
                     <td>{{str_limit($post->body,10)}}</td>
+                    <td><a href="{{route('home.post',$post->id)}}">View Post</a></td>
+                    <td><a href="{{route('admin.comments.show',$post->id)}}">View Comments</a></td>
                     <td>{{$post->created_at->diffForHumans()}}</td>
                     <td>{{$post->updated_at->diffForHumans()}}</td>
                 </tr>
