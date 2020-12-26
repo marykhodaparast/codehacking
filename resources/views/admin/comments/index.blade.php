410a10ee36c531fd3ccfa3d45043f9d4ea02b4d6
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-26 14:09:30 +0330

    Displaying posts 31-260

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -1,4 +1,28 @@
 @extends('layouts.admin')
 @section('content')
-    <h1>Comments</h1>
+    @if(count($comments) > 0)
+        <table class="table">
+            <thead>
+            <tr>
+                <th>id</th>
+                <th>Author</th>
+                <th>Email</th>
+                <th>Body</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($comments as $comment)
+                <tr>
+                    <td>{{$comment->id}}</td>
+                    <td>{{$comment->author}}</td>
+                    <td>{{$comment->email}}</td>
+                    <td>{{$comment->body}}</td>
+                    <td><a href="{{route('home.post',$comment->post->id)}}">View Post</a></td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @else
+        <h1 class="text-center">No comments</h1>
+    @endif
 @endsection
