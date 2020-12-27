e1726e26108388349d6d38b6d169eae33d244c5d
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-27 14:06:29 +0330

    Displaying replies 31-268

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -64,33 +64,33 @@
                         <small>{{$comment->created_at->diffForHumans()}}</small>
                     </h4>
                 {{$comment->body}}
-                <!-- Nested Comment -->
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Nested Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
-                            commodo.
-                            Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
-                            ac nisi
-                            vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
-                        {!! Form::open(['method' => 'post', 'action' => 'CommentRepliesController@createReply']) !!}
-                        <input type="hidden" name="comment_id" value="{{$comment->id}}">
-                        <div class="form-group">
-                            {!! Form::label('body','Body:') !!}
-                            {!! Form::textarea('body',null,['class' => 'form-control','rows' => 1]) !!}
-                        </div>
-                        <div class="form-group">
-                            {!! Form::submit('submit',['class' => 'btn btn-primary']) !!}
-                        </div>
-                        {!! Form::close() !!}
-                    </div>
+                @if(count($comment->replies)>0)
+                    <!-- Nested Comment -->
+                        @foreach($comment->replies as $reply)
+                            <div class="media" id="nested-comment">
+                                <a class="pull-left" href="#">
+                                    <img class="media-object" src="{{$reply->photo}}" alt="" height="64">
+                                </a>
+                                <div class="media-body">
+                                    <h4 class="media-heading">{{$reply->author}}
+                                        <small>{{$reply->created_at->diffForHumans()}}</small>
+                                    </h4>
+                                    {{$reply->body}}
+                                </div>
+                                {!! Form::open(['method' => 'post', 'action' => 'CommentRepliesController@createReply']) !!}
+                                <input type="hidden" name="comment_id" value="{{$comment->id}}">
+                                <div class="form-group">
+                                    {!! Form::label('body','Body:') !!}
+                                    {!! Form::textarea('body',null,['class' => 'form-control','rows' => 1]) !!}
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::submit('submit',['class' => 'btn btn-primary']) !!}
+                                </div>
+                                {!! Form::close() !!}
+                            </div>
+                        @endforeach
                     <!-- End Nested Comment -->
+                    @endif
                 </div>
             </div>
         @endforeach
