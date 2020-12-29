e4807fa47b1bc36e1221dd5f8795cf9941bf8edf
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-29 16:06:19 +0330

    Upgrading routes part1 33-280

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,17 @@
 */
 
 Route::get('/', function () {
-    return 'hello';
+    return view('welcome');
 });
-Route::get('/admin', function () {
-    return view('admin.index');
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('/admin', function () {
+        return view('admin.index');
+    });
+    Route::resource('admin/users', 'AdminUsersController');
+    Route::resource('admin/posts', 'AdminPostsController');
+    Route::resource('admin/categories', 'AdminCategoriesController');
+    Route::resource('admin/media', 'AdminMediasController');
+    Route::any('admin/media/upload', ['as' => 'admin.media.upload', 'uses' => 'AdminMediasController@store']);
+    Route::resource('admin/comments', 'PostCommentsController');
+    Route::resource('admin/comment/replies', 'CommentRepliesController');
 });
