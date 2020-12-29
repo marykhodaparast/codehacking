edf76a2363e60bc286c5b46b279ef4a620f009dd
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-29 17:09:28 +0330

    Upgrading users 33-283

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -32,7 +32,7 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        $roles = Role::lists('name', 'id')->all();//it gives us an array
+        $roles = Role::pluck('name', 'id')->all();//it gives us an array
         return view('admin.users.create', compact('roles'));
     }
 
@@ -85,7 +85,7 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         $user = User::findOrFail($id);
-        $roles = Role::lists('name', 'id')->all();
+        $roles = Role::pluck('name', 'id')->all();
         return view('admin.users.edit', compact('user', 'roles'));
     }
 
