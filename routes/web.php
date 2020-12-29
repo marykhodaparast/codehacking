b9ea233151119d6942db250012b802a83b190229
maryam khodaparast <khcomputergirl@gmail.com>
2020-12-29 16:54:14 +0330

    Upgrading routes part3 33-282

--- routes/web.php
+++ routes/web.php
@@ -13,30 +13,60 @@
 
 use Illuminate\Support\Facades\Auth;
 
-if(version_compare(PHP_VERSION, '7.2.0', '>=')) {
+if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
     error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 }
 Route::get('/', function () {
     return view('welcome');
 });
 Auth::Routes();
-Route::get('/logout','Auth\LoginController@logout');
+Route::get('/logout', 'Auth\LoginController@logout');
 Route::get('/home', 'HomeController@index');
 
-Route::get('/post/{id}',['as' => 'home.post', 'uses'=>'AdminPostsController@post']);
+Route::get('/post/{id}', ['as' => 'home.post', 'uses' => 'AdminPostsController@post']);
 
 Route::group(['middleware' => 'admin'], function () {
     Route::get('/admin', function () {
         return view('admin.index');
     });
-    Route::resource('admin/users', 'AdminUsersController');
-    Route::resource('admin/posts', 'AdminPostsController');
-    Route::resource('admin/categories', 'AdminCategoriesController');
-    Route::resource('admin/media', 'AdminMediasController');
+    Route::resource('admin/users', 'AdminUsersController', ['names' => [
+        'index' => 'admin.users.index',
+        'create' => 'admin.users.create',
+        'store' => 'admin.users.store',
+        'edit' => 'admin.users.edit',
+    ]]);
+    Route::resource('admin/posts', 'AdminPostsController', ['names' => [
+        'index' => 'admin.posts.index',
+        'create' => 'admin.posts.create',
+        'store' => 'admin.posts.store',
+        'edit' => 'admin.posts.edit',
+    ]]);
+    Route::resource('admin/categories', 'AdminCategoriesController', ['names' => [
+        'index' => 'admin.categories.index',
+        'create' => 'admin.categories.create',
+        'store' => 'admin.categories.store',
+        'edit' => 'admin.categories.edit',
+    ]]);
+    Route::resource('admin/media', 'AdminMediasController', ['names' => [
+        'index' => 'admin.media.index',
+        'create' => 'admin.media.create',
+        'store' => 'admin.media.store',
+        'edit' => 'admin.media.edit',
+    ]]);
     Route::any('admin/media/upload', ['as' => 'admin.media.upload', 'uses' => 'AdminMediasController@store']);
-    Route::resource('admin/comments', 'PostCommentsController');
-    Route::resource('admin/comment/replies', 'CommentRepliesController');
+    Route::resource('admin/comments', 'PostCommentsController', ['names' => [
+        'index' => 'admin.comments.index',
+        'create' => 'admin.comments.create',
+        'store' => 'admin.comments.store',
+        'edit' => 'admin.comments.edit',
+    ]]);
+    Route::resource('admin/comment/replies', 'CommentRepliesController', ['names' => [
+        'index' => 'admin.replies.index',
+        'create' => 'admin.replies.create',
+        'store' => 'admin.replies.store',
+        'edit' => 'admin.replies.edit',
+    ]]);
 });
-Route::group(['middleware' => 'auth'],function (){
-    Route::post('comment/reply','CommentRepliesController@createReply');
+Route::group(['middleware' => 'auth'], function () {
+    Route::post('comment/reply', 'CommentRepliesController@createReply');
 });
