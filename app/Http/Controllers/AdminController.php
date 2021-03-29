a22815926329904344de31020bba486947dacb65
maryam khodaparast <khcomputergirl@gmail.com>
2021-03-29 15:54:17 +0430

    replacing chart static data with dynamic

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Comment;
+use App\Category;
 
 class AdminController extends Controller
 {
     public function index(){
-        
-       return view('admin.index');
+       
+       $postsCount = Post::count(); 
+       $commentsCount = Comment::count();
+       $categoriesCount = Category::count();
+
+       return view('admin.index',compact('postsCount','commentsCount','categoriesCount'));
     }
 
 }
